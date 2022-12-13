ab76b673f47d65312bd240a4a07b59ae5c513cf6
Thanapat <koop0408@gmail.com>
2022-12-13 16:23:35 +0700

    edit title

--- lib/connect_ip.dart
+++ lib/connect_ip.dart
@@ -33,7 +33,7 @@ class _MyHomePageState extends State<MyHomePage>
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("ScoreBoard DEMO123"),
+        title: const Text("ScoreBoard DEMO"),
       ),
       body: Center(
         child: Column(
