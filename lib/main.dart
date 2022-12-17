70211e835b09084b5b87b61bcd7c19df8b2031f5
Phumipat2000 <gaymanXIII@gmail.com>
2022-12-17 15:47:58 +0700

    DebugBannerOut

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "ScoreBoard",
       home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.brown),
     );
   }
