f8f847a6d7ae7c18e312af7a725cd7b7d3e39639
Phumipat2000 <gaymanXIII@gmail.com>
2022-12-22 22:50:31 +0700

    Split Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'dart:io' show Platform;
+import 'package:scoreboard/Components/IntroSplashScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,87 +19,6 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class Splash extends StatefulWidget {
-  const Splash({super.key});
-
-  @override
-  State<Splash> createState() => _SplashState();
-}
-
-class _SplashState extends State<Splash> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const MyHomePage()));
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.amber,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'image/scoreboard.png',
-              height: 130,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            if (Platform.isAndroid)
-              const CupertinoActivityIndicator(
-                radius: 20,
-              )
-            else
-            const CircularProgressIndicator(
-              color: Colors.white,
-            ),
-            const Text(
-              '\nMultipurpose IoT Scoreboard App',
-              style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-// class SplashScreen extends StatelessWidget {
-//   const SplashScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return AnimatedSplashScreen(
-//       splash: Column(
-//         children: [
-//           Image.asset('image/scoreboard.png'),
-//           const Text(
-//             'Multipurpose IoT Scoreboard App',
-//             style: TextStyle(
-//                 fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),
-//           )
-//         ],
-//       ),
-//       backgroundColor: Colors.white,
-//       nextScreen: const MyHomePage(),
-//       splashIconSize: 700,
-//       duration: 2000,
-//       splashTransition: SplashTransition.fadeTransition,
-//       pageTransitionType: PageTransitionType.rightToLeftWithFade,
-//     );
-//   }
-// }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
