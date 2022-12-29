c5dd67b39f1eae74539fe877611584b1719a3940
Phumipat2000 <gaymanXIII@gmail.com>
2022-12-29 11:48:31 +0700

    Add MenuSports

---
+++ lib/Components/MenuSports.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class MenuSports extends StatefulWidget {
+  const MenuSports({super.key});
+
+  @override
+  State<MenuSports> createState() => _MenuSportsState();
+}
+
+class _MenuSportsState extends State<MenuSports> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.brown,
+      width: double.infinity,
+      height: 140,
+      padding: EdgeInsets.only(top: 20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            height: 60,
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              image: DecorationImage(
+                image: AssetImage('image/scoreboard.png'),
+              ),
+            ),
+          ),
+          Text(
+            "ScoreBoard",
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+          Text(
+            "List Sports",
+            style: TextStyle(color: Colors.white, fontSize: 14),
+          ),
+        ],
+      ),
+    );
+  }
+}
