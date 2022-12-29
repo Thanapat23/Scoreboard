86069b969f91d5620ea365673daf4e9e00c6a954
Phumipat2000 <gaymanXIII@gmail.com>
2022-12-29 17:54:21 +0700

    Page ScoreBoard Volleyball

---
+++ lib/Components/Volleyball.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Volleyball extends StatefulWidget {
+  @override
+  _VolleyballState createState() => _VolleyballState();
+}
+
+class _VolleyballState extends State<Volleyball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Volleyball"),
+      ),
+    );
+  }
+}
