24a241b4ac8d92bbdca69007d8a357d621d7e531
Phumipat2000 <gaymanXIII@gmail.com>
2022-12-29 17:54:04 +0700

    Page ScoreBoard Football

---
+++ lib/Components/Football.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Football extends StatefulWidget {
+  @override
+  _FootballState createState() => _FootballState();
+}
+
+class _FootballState extends State<Football> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Football"),
+      ),
+    );
+  }
+}
