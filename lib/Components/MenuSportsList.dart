8afc6bc73d6bfd7b353e3823f98467ddba90dc74
Phumipat2000 <gaymanXIII@gmail.com>
2022-12-29 17:56:36 +0700

    ทำแยกจาก main ยังไม่ได้

--- lib/Components/MenuSportsList.dart
+++ lib/Components/MenuSportsList.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
+import '../main.dart';
+import 'MenuItems.dart';
+
+var currentPage = DrawerSections.home;
 
 Widget MenuSportsList() {
-  return Container();
+  return Container(
+    padding: EdgeInsets.only(
+      top: 15,
+    ),
+    child: Column(
+      //Show the list of menu drawer
+      children: [
+        menuItems(1, "Home", Icons.home_outlined,
+            currentPage == DrawerSections.home ? true : false),
+        menuItems(2, "Basketball", Icons.sports_basketball_outlined,
+            currentPage == DrawerSections.basketball ? true : false),
+        menuItems(3, "Volleyball", Icons.sports_volleyball_outlined,
+            currentPage == DrawerSections.volleyball ? true : false),
+        menuItems(4, "Football", Icons.sports_soccer_outlined,
+            currentPage == DrawerSections.football ? true : false),
+      ],
+    ),
+  );
 }
